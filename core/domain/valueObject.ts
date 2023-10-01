d859bed0a61f9db228a6edea43557b5627f4de3b
Fawzi Abdulfattah <iifawzie@gmail.com>
2023-10-02 00:31:29 +0300

    chore/core: valueObject might contain multiple values, hence introducing props
    Signed-off-by: Fawzi Abdulfattah <iifawzie@gmail.com>

--- core/domain/valueObject.ts
+++ core/domain/valueObject.ts
@@ -1,13 +1,23 @@
-export abstract class ValueObject<T> {
-    constructor(public readonly value: T) {
-        this.value = value;
+interface ValueObjectProps {
+  [index: string]: any;
+}
+
+export abstract class ValueObject<T extends ValueObjectProps> {
+  public readonly props: T;
+  
+  constructor(props: T) {
+    let baseProps: any = {
+      ...props,
+    };
+
+    this.props = baseProps;
+  }
+
+  equals(vo: ValueObject<T>): boolean {
+    if (vo == null || vo == undefined || vo.props === undefined) {
+      return false;
     }
 
-    equals(value: ValueObject<T>): boolean {
-        if (value == null || value == undefined) {
-            return false;
-        }
-
-        return value === this.value;
-    }
+    return JSON.stringify(vo.props) === JSON.stringify(this.props);
+  }
 }
