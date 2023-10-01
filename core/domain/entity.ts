98691bf12fe77fa53c1bcff53537b6d824d9a106
Fawzi Abdulfattah <iifawzie@gmail.com>
2023-10-01 22:40:40 +0300

    chore/core: adding entity class
    Signed-off-by: Fawzi Abdulfattah <iifawzie@gmail.com>

---
+++ core/domain/entity.ts
@@ -0,0 +1,27 @@
+import { UniqueID } from "./unique.identifier";
+
+const isEntity = (entity: any): entity is Entity<any> => {
+    return entity instanceof Entity; 
+}
+
+export abstract class Entity<T> {
+    protected readonly id: UniqueID
+    protected readonly props: T
+
+    constructor(props: T, id?: UniqueID) {
+        this.id = id ? id : new UniqueID();
+        this.props = props;
+    }
+
+    equals(entity: Entity<T>) {
+        if (entity == null || entity == undefined) {
+            return false;
+        }
+
+        if (!isEntity(entity)) {
+            return false;
+        }
+
+        return this.id.equals(entity.id);
+    }
+}
